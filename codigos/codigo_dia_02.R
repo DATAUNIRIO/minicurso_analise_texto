c2ae429009ea3ca760dcf3de326a1528e0ce2e2d
DATAUNIRIO <duttross@gmail.com>
2023-04-10 18:48:26 -0300

    Update codigo_dia_02.R

--- codigos/codigo_dia_02.R
+++ codigos/codigo_dia_02.R
@@ -40,4 +40,31 @@ library(wordcloud2)
 wordcloud2(demoFreq)
 #wordcloud2(contagem)
 
+library(corpus)
+text <- "love loving lovingly loved lover lovely love"
+text
+
+text_tokens(text) 
+
+# Stemming é o ato de remover inflexões de uma palavra 
+text_tokens(text, stemmer = "en") # english stemmer
+
+
+
+texto <- "caminhar caminhando caminhou andou andar andando"
+texto
+text_tokens(texto) 
+text_tokens(texto, stemmer = "pt") 
+
+#remotes::install_github("DATAUNIRIO/lemmar")
+library(lemmar)
+lemmatize_pt(texto)
+lemmatize_pt(Poeminho_do_Contra)
+
+
+#library(spacyr)
+parsedtxt <- spacy_parse(texto)
+parsedtxt
+
+spacy_parse(Poeminho_do_Contra)
 
